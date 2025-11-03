277c4cb26d46b9858d573d9c0a391a5ab745ae91
Duvan Sánchez <dysgtechnology@gmail.com>
2025-11-03 06:25:41 -0500

    Logro: Construcción de los endpoints
    Realizado:
    - Creación del archivo routers/catalogo_ruta.py
    - Implementación de los endpoints para el catálogo de productos:
        - Leer la raíz del catálogo
        - Leer productos con filtros
        - Agregar un producto
        - Actualizar un producto
        - Eliminar un producto

--- routers/catalogo_ruta.py
+++ routers/catalogo_ruta.py
@@ -4,9 +4,29 @@ router = APIRouter(
     prefix="/catalogo",
 )
 
+#---Leer la raiz del catalogo---#
 @router.get("/")
 async def leer_catalogo():
     return {
         "mensaje": "Catálogo de productos"
             }
 
+#---Leer prductos con filtros---#
+@router.get("/productos")
+async def productos(marca: list[str] = [], categoria: list[str] = [], precio_min : int | None = None, precio_max: int | None = None, ordenar_precio: str | None = None):
+    return "" 
+
+#---Agregar un producto---#    
+@router.post("/productos/agregar")
+async def agregar_producto():
+    return""
+
+#---Actualizar un producto---#
+@router.put("/productos/actualizar")
+async def actualizar_producto():
+    return""
+
+#---Eliminar un producto---#
+@router.delete("/productos/eliminar")
+async def eliminar_producto():
+    return""
